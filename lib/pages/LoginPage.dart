514142698ae14f167f939cf154492d306900da6d
shurik9903 <59256714+shurik9903@users.noreply.github.com>
2023-08-03 17:50:12 +0700

    add about the program window

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_univ/theme/AppThemeDefault.dart';
 import '../modules/LoginFetch.dart';
 
 class LoginPage extends StatefulWidget {
@@ -15,60 +16,73 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          Container(
-            width: 500,
-            margin: const EdgeInsets.symmetric(vertical: 100, horizontal: 100),
-            child: Column(
-              children: [
-                Wrap(
-                  runSpacing: 10,
-                  children: [
-                    TextField(
-                      onChanged: (value) {
+    return Column(
+      children: [
+        Container(
+          width: 500,
+          margin: const EdgeInsets.symmetric(vertical: 100, horizontal: 100),
+          padding: EdgeInsets.all(20),
+          color: appTheme(context).secondaryColor,
+          child: Column(
+            children: [
+              Wrap(
+                runSpacing: 10,
+                children: [
+                  TextField(
+                    onChanged: (value) {
+                      setState(() {
+                        login = value;
+                      });
+                    },
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(), labelText: 'Логин'),
+                  ),
+                  TextField(
+                    onChanged: (value) {
+                      setState(() {
+                        password = value;
+                      });
+                    },
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(), labelText: 'Пароль'),
+                  ),
+                  Text(msg)
+                ],
+              ),
+              Row(
+                children: [
+                  TextButton(
+                    onPressed: () async {
+                      loginFetch(login, password).then((value) {
+                        Navigator.pushNamed(context, '/work');
+                      }).catchError((error) {
                         setState(() {
-                          login = value;
+                          msg = error.toString();
                         });
-                      },
-                      decoration: const InputDecoration(
-                          border: OutlineInputBorder(), labelText: 'Логин'),
-                    ),
-                    TextField(
-                      onChanged: (value) {
-                        setState(() {
-                          password = value;
-                        });
-                      },
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                          border: OutlineInputBorder(), labelText: 'Пароль'),
-                    ),
-                    Text(msg)
-                  ],
-                ),
-                Row(
-                  children: [
-                    TextButton(
-                      onPressed: () async {
-                        loginFetch(login, password).then((value) {
-                          Navigator.pushNamed(context, '/work');
-                        }).catchError((error) {
-                          setState(() {
-                            msg = error.toString();
-                          });
-                        });
-                      },
-                      child: const Text("Вход"),
-                    )
-                  ],
-                ),
-              ],
-            ),
+                      });
+                    },
+                    child: const Text("Вход"),
+                  )
+                ],
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+        const Spacer(),
+        Container(
+          width: double.infinity,
+          height: 50,
+          color: appTheme(context).secondaryColor,
+          child: const Row(
+            children: [
+              Text(
+                "       Регистрационный номер: 2023665649",
+              ),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
