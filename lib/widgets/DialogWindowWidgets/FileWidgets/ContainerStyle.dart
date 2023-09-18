df564c755ce29d34584a9aff6a5f2806e58644f4
shurik9903 <59256714+shurik9903@users.noreply.github.com>
2023-09-18 18:37:21 +0700

    update

--- lib/widgets/DialogWindowWidgets/FileWidgets/ContainerStyle.dart
+++ lib/widgets/DialogWindowWidgets/FileWidgets/ContainerStyle.dart
@@ -2,36 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:flutter_univ/theme/AppThemeDefault.dart';
 
 class ContainerStyle extends StatefulWidget {
-  const ContainerStyle({super.key, this.child, this.text, this.bottom});
+  const ContainerStyle(
+      {super.key,
+      this.child,
+      this.text = '',
+      this.bottom = false,
+      this.borderColor,
+      this.color});
 
   final String? text;
   final Widget? child;
-  final bool? bottom;
+  final bool bottom;
+  final Color? borderColor;
+  final Color? color;
 
   @override
   State<ContainerStyle> createState() => _ContainerStyleState();
 }
 
 class _ContainerStyleState extends State<ContainerStyle> {
-  late String? text;
-  late bool bottom;
-
-  @override
-  void initState() {
-    super.initState();
-    text = widget.text;
-    bottom = widget.bottom ?? false;
-  }
+  late Color borderColor;
+  late Color color;
 
   @override
   Widget build(BuildContext context) {
-    final border = bottom
+    borderColor = widget.borderColor ?? appTheme(context).tertiaryColor;
+    color = widget.color ?? appTheme(context).secondaryColor;
+
+    final border = widget.bottom
         ? Border(
-            top: BorderSide(color: appTheme(context).tertiaryColor, width: 1),
-            bottom:
-                BorderSide(color: appTheme(context).tertiaryColor, width: 1))
+            top: BorderSide(color: borderColor, width: 1),
+            bottom: BorderSide(color: borderColor, width: 1))
         : Border(
-            top: BorderSide(color: appTheme(context).tertiaryColor, width: 1),
+            top: BorderSide(color: borderColor, width: 1),
           );
 
     return Stack(
@@ -48,12 +51,12 @@ class _ContainerStyleState extends State<ContainerStyle> {
         Positioned(
           left: 25,
           top: 2,
-          child: text != null
+          child: widget.text != null
               ? Container(
                   padding: const EdgeInsets.only(bottom: 1, left: 5, right: 5),
-                  color: appTheme(context).secondaryColor,
+                  color: color,
                   child: Text(
-                    text!,
+                    widget.text!,
                     style: const TextStyle(fontSize: 14),
                   ),
                 )
